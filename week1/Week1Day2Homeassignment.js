35ae2a10feb6d51a976c5b5b1bab6fac22af517a
Mithra0498 <mithras0909@gmail.com>
2026-07-08 22:26:30 +0530

    Add files via upload
    Week1Day2Homeassignment

---
+++ week1/Week1Day2Homeassignment.js
@@ -0,0 +1,25 @@
+
+// String
+let userName = "Vijayarajan";
+console.log("userName:", userName);
+console.log("Type of userName:", typeof userName);
+
+// String
+let organization = "Testleaf";
+console.log("organization:", organization);
+console.log("Type of organization:", typeof organization);
+
+// Number
+let contactNumber = 9876543210;
+console.log("contactNumber:", contactNumber);
+console.log("Type of contactNumber:", typeof contactNumber);
+
+// Boolean
+let knowsAutomation = true;
+console.log("knowsAutomation:", knowsAutomation);
+console.log("Type of knowsAutomation:", typeof knowsAutomation);
+
+// Undefined
+let usesPlaywright;
+console.log("usesPlaywright:", usesPlaywright);
+console.log("Type of usesPlaywright:", typeof usesPlaywright);
